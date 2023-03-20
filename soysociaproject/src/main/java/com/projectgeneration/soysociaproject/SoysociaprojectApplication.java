2a31001321b934b510e90d56beb2ecee2655358f
jprivas30 <juanpr2512@gmail.com>
2023-03-20 13:23:51 -0300

    first commit

---
+++ soysociaproject/src/main/java/com/projectgeneration/soysociaproject/SoysociaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.projectgeneration.soysociaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoysociaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoysociaprojectApplication.class, args);
+	}
+
+}
---
+++ soysociaproject/src/test/java/com/projectgeneration/soysociaproject/SoysociaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectgeneration.soysociaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoysociaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
